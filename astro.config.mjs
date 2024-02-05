42512af23450cdfe792424fcaac2514f3a857630
deveaston06 <deveaston06@gmail.com>
2024-02-06 01:19:57 +0800

    try hybrid output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ const SERVER_PORT = 4321;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://elprog-blog.vercel.app";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -37,7 +37,7 @@ export default defineConfig({
 			config: { applyBaseStyles: false },
 		}),
 	],
-	output: "server",
+	output: "hybrid",
 	adapter: vercel({
 		webAnalytics: {
 			enabled: true,
