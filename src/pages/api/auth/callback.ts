a89719f75cbd0fb0f2a54a5737d1fbfb335a870c
deveaston06 <deveaston06@gmail.com>
2024-02-07 14:27:01 +0800

    try to fix redirect url for callback

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -1,18 +1,6 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
 
-const getURL = () => {
-	let url =
-		process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-		process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-		"http://localhost:4321/";
-	// Make sure to include `https://` when not localhost.
-	url = url.includes("http") ? url : `https://${url}`;
-	// Make sure to include a trailing `/`.
-	url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-	return url;
-};
-
 export const GET: APIRoute = async ({ url, cookies, redirect }) => {
 	const authCode = url.searchParams.get("code");
 
@@ -38,5 +26,5 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
 		secure: true,
 		httpOnly: true,
 	});
-	return redirect(getURL());
+	return redirect("/");
 };
