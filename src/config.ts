914cccc7058bfb446fde2036cd668e819d426ab2
deveaston06 <deveaston06@gmail.com>
2024-02-07 13:29:07 +0800

    auth apis and fix minor bugs

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "ELPROG Developer Blog";
+export const SITE_TITLE = "ELPROG Blog";
 export const SITE_DESCRIPTION = "Welcome to my blog! I write about modern web development.";
 export const TWITTER_HANDLE = "@deveaston06";
 export const MY_NAME = "DEV EASTON";
